d76ab30ca99161ceb5c40494c26686525b9b5135
Michael Tighe <strideriidx@gmail.com>
2020-08-06 10:36:37 -0700

    Remove default webapi endpoints/models

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 Running `docker-compose up` will start the MSSQL Server
 
 See individual project READMEs for further detail
+
+## Project Layout
+
+```
+./
+ requests - Postman request / environment collections
+ src      - Source code for indivdual projects
+ vol      - Docker volume mount root
+    /sql  - MSSQL volume
+```
---
+++ src/Rhyze.API/Controllers/MeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Rhyze.API.Controllers
+{
+    [ApiController]
+    public class MeController : ControllerBase
+    {
+        [Route("/me")]
+        public object Me()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var id = User.FindFirstValue("user_id");
+
+            return new { id, email };
+        }
+    }
+}
