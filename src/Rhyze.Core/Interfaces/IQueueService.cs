0b60b2d7b4c5961711707e338e63e07a74a23583
Michael Tighe <strideriidx@gmail.com>
2020-08-08 20:41:33 -0700

    Add IQueueService contract to Core project

---
+++ src/Rhyze.Core/Interfaces/IQueueService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Rhyze.Core.Interfaces
+{
+    /// <summary>
+    /// Contract for enqueuing the processing of specific actions
+    /// </summary>
+    public interface IQueueService
+    {
+        /// <summary>
+        /// Enqueues the deletion of a given album
+        /// </summary>
+        /// <param name="albumName"></param>
+        /// <returns></returns>
+        Task EnqueueAlbumDeletion(string albumName);
+    }
+}
