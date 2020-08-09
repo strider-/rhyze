e4cba34467e228d3042eac9a165d0c817b2ce811
Michael Tighe <strideriidx@gmail.com>
2020-08-08 21:45:21 -0700

    Rename migration source files to prefix with migration number

---
+++ src/Rhyze.Data/Migrations/2020_08_06_01_CreateUsersTable.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+
+namespace Rhyze.Data.Migrations
+{
+    [Migration(2020_08_06_01)]
+    public class CreateUsersTable : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"CREATE TABLE [dbo].[Users] (
+	                        [Id]	      UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+	                        [IdentityId]  NVARCHAR(255)    NOT NULL,
+	                        [Email]       NVARCHAR(255)    NOT NULL,
+	                        CONSTRAINT [PK_Users] PRIMARY KEY NONCLUSTERED ([Id] ASC)
+                        );");
+
+            Create.Index("IX_IdentityId_Email")
+                 .OnTable("Users")
+                 .OnColumn("IdentityId").Ascending()
+                 .OnColumn("Email").Ascending()
+                 .WithOptions().Clustered();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Users");
+        }
+    }
+}
---
+++ src/Rhyze.Data/Migrations/2020_08_07_01_CreateTracksTable.cs
@@ -0,0 +1,39 @@
+﻿using FluentMigrator;
+
+namespace Rhyze.Data.Migrations
+{
+    [Migration(2020_08_07_01)]
+    public class CreateTracksTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Tracks")
+                  .WithColumn("Id").AsGuid().PrimaryKey().NotNullable().WithDefaultValue(SystemMethods.NewSequentialId)
+                  .WithColumn("Title").AsString(255).NotNullable().Indexed()
+                  .WithColumn("Album").AsString(255).Indexed()
+                  .WithColumn("Artist").AsString(255).Indexed()
+                  .WithColumn("AlbumArtist").AsString(500)
+                  .WithColumn("TrackNo").AsInt32().NotNullable()
+                  .WithColumn("TrackCount").AsInt32().NotNullable()
+                  .WithColumn("DiscNo").AsInt32().NotNullable()
+                  .WithColumn("DiscCount").AsInt32().NotNullable()
+                  .WithColumn("Year").AsInt32()
+                  .WithColumn("Duration").AsDouble().NotNullable()
+                  .WithColumn("ImageUrl").AsString(500)
+                  .WithColumn("AudioUrl").AsString(500).NotNullable()
+                  .WithColumn("UploadedUtc").AsDateTime().NotNullable().WithDefaultValue(SystemMethods.CurrentUTCDateTime)
+                  .WithColumn("LastPlayedUtc").AsDateTime().Nullable()
+                  .WithColumn("PlayCount").AsInt32().NotNullable()
+                  .WithColumn("OwnerId").AsGuid().NotNullable().Indexed();
+
+            Create.ForeignKey("FK_Tracks_To_User")
+                  .FromTable("Tracks").ForeignColumn("OwnerId")
+                  .ToTable("Users").PrimaryColumn("Id");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Tracks");
+        }
+    }
+}
